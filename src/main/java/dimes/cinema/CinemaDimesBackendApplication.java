f20cd8ddf92b9780a2cad04cb33c8515ee3c5510
scotch2710 <gallofabrizio98@gmail.com>
2025-06-20 15:50:22 +0200

    1

---
+++ src/main/java/dimes/cinema/CinemaDimesBackendApplication.java
@@ -0,0 +1,13 @@
+package dimes.cinema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemaDimesBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinemaDimesBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dimes/cinema/CinemaDimesBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package dimes.cinema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemaDimesBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
