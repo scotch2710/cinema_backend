8c07f566e4e8004e98e66c2905bf162a537ea9ff
scotch2710 <gallofabrizio98@gmail.com>
2025-06-26 00:20:26 +0200

    immagini

--- src/main/java/dimes/cinema/config/SecurityConfig.java
+++ src/main/java/dimes/cinema/config/SecurityConfig.java
@@ -19,21 +19,21 @@ public class SecurityConfig {
         http
             .csrf(csrf -> csrf.disable())
             .authorizeHttpRequests(auth -> auth
-                .requestMatchers(HttpMethod.GET, "/api/films", "/api/spettacoli").permitAll()
+                .requestMatchers(HttpMethod.GET, "/api/films", "/api/spettacoli", "/images/**").permitAll()
                 .anyRequest().authenticated()
             )
-            // --- MODIFICA INIZIA QUI ---
+            
             .oauth2ResourceServer(oauth2 -> oauth2
                 .jwt(jwt -> jwt
                     .jwtAuthenticationConverter(jwtAuthenticationConverter()) // Usa il nostro convertitore custom
                 )
             );
-            // --- MODIFICA FINISCE QUI ---
+            
 
         return http.build();
     }
 
-    // --- NUOVO METODO AGGIUNTO ---
+    
     // Questo metodo crea il convertitore e gli dice di usare la nostra classe
     // KeycloakRoleConverter per estrarre i ruoli/permessi dal token.
     @Bean
