2a068832eb0e523d3b9defa74d11eafdd688c8d3
scotch2710 <gallofabrizio98@gmail.com>
2025-07-18 01:36:13 +0200

    form admin

---
+++ src/main/java/dimes/cinema/controllers/SalaController.java
@@ -0,0 +1,31 @@
+package dimes.cinema.controllers;
+
+import dimes.cinema.entities.Sala;
+import dimes.cinema.repositories.SalaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/sale")
+@CrossOrigin(origins = "http://localhost:4200") // Permette le chiamate dal frontend
+public class SalaController {
+
+    @Autowired
+    private SalaRepository salaRepository;
+
+    /**
+     * Endpoint per ottenere la lista di tutte le sale cinematografiche.
+     * @return Una lista di tutte le sale.
+     */
+    @GetMapping
+    public ResponseEntity<List<Sala>> getAllSale() {
+        List<Sala> sale = salaRepository.findAll();
+        return ResponseEntity.ok(sale);
+    }
+}
